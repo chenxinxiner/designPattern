5827e43a932f414b360072623be52208f81f2411
chenxinxiner <1074537862@qq.com>
2020-03-12 16:29:03 +0800

    java实现设计模式.初始化

---
+++ src/main/java/strategyPattern/Duck.java
@@ -0,0 +1,13 @@
+package strategyPattern;
+
+/**
+ * 鸭子
+ *
+ * @author ：chenxin
+ * @date ：Created in 2020-03-12 16:27
+ * @description：鸭子
+ * @modified By：
+ * @version: 1.-$
+ */
+public class Duck {
+}
